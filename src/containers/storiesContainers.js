d99a6eb5c4205639e6de069e48be0af598234ca0
adetoro ezekiel <adetoroe787@gmail.com>
2022-08-10 14:07:22 -0400

    styled

--- src/containers/storiesContainers.js
+++ src/containers/storiesContainers.js
@@ -1,6 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { Story } from "../components/Story";
 import { getStoryIds, getStory } from "../services/hnapi";
+import {
+  StoryContainerWrapper,
+  GlobalStyle,
+} from "../styles/storyContainerStyle";
 
 export const StoriesContainer = () => {
   const [storyIds, setStoryIds] = useState([]);
@@ -9,7 +13,15 @@ export const StoriesContainer = () => {
     getStoryIds().then((data) => setStoryIds(data));
   }, []);
 
-  return storyIds.map(storyId => (
-    <Story key={storyId} storyId={storyId}/>
-  ))
+  return (
+    <>
+      <GlobalStyle />
+      <StoryContainerWrapper data-test-id="stories-container">
+        <h1>Hacker News Stories</h1>
+        {storyIds.map((storyId) => (
+          <Story key={storyId} storyId={storyId} />
+        ))}
+      </StoryContainerWrapper>
+    </>
+  );
 };
